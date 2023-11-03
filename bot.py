aea0c933aa03501312715400600f5a6508eae4c8
Mihail Vainer <mh93@yandex.ru>
2023-11-04 01:23:42 +0700

    04112023

--- bot.py
+++ bot.py
@@ -9,14 +9,52 @@ import config
 TOKEN = "6916850744:AAF3bgfcmOtmIfp-Tn7nC2NEViMQ_97rLJs"
 bot = telebot.TeleBot(TOKEN)
 
-
-
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(config.chatid, 'Привет бро, я бот для мониторинга твоего сервера! Что будем мониторить? \n'
-                                    '/id - ChatID бота\n'
-                                    '/cpu - инфа о процессоре\n')
+def start1(message):
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    btn1 = types.InlineKeyboardButton(text='Сведения об ОС', callback_data='btn1')
+    btn2 = types.InlineKeyboardButton(text='Значение LA', callback_data='btn2')
+    btn3 = types.InlineKeyboardButton(text='Место на диске', callback_data='btn3')
+    btn4 = types.InlineKeyboardButton(text='Количество inodes', callback_data='btn4')
+    btn5 = types.InlineKeyboardButton(text='Время работы системы', callback_data='btn5')
+    kb.add(btn1, btn2, btn3, btn4, btn5)
+    bot.send_message(config.chatid, 'Привет бро! Что будем мониторить?', reply_markup=kb)
 
+@bot.callback_query_handler(func = lambda call: True)
+def inlinekeyboards(call):
+    if call.from_user.id == config.chatid:
+        if call.data == "btn1":
+            a = check_output(args='uname -a', shell=True)
+            bot.send_message(config.chatid, a)
+        elif call.data == "btn2":
+            b = list(psutil.getloadavg())
+            b = f'1 мин - {b[0]}, 5 мин - {b[1]}, 15 мин - {b[2]}'
+            bot.send_message(config.chatid, b)
+        elif call.data == "btn3":
+            c = list(psutil.disk_usage('/'))
+            c = f'Диск занят на - {c[3]} %, доступно {int(c[2]/1024/1024/1024)} Гб'
+            bot.send_message(config.chatid, c)
+        if call.data == "btn4":
+            d = check_output(args='df -i', shell=True)
+            bot.send_message(config.chatid, d)
+        if call.data == "btn5":
+            e = check_output(args='uptime -p', shell=True)
+            bot.send_message(config.chatid, e)
+
+
+
+
+'''@bot.message_handler(commands=['start'])
+def start(message):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=4)
+    btn1 = types.KeyboardButton(text='uname -a')
+    btn2 = types.KeyboardButton(text='/cpu')
+    btn3 = types.KeyboardButton(text='df -h')
+    btn4 = types.KeyboardButton(text='df -i')
+    btn5 = types.KeyboardButton(text='uptime')
+    kb.add(btn1, btn2, btn3, btn4, btn5)
+    bot.send_message(config.chatid, 'Привет бро, я бот для мониторинга твоего сервера! Что будем мониторить?', reply_markup=kb)'''
+'''
 @bot.message_handler(commands=['id'])
 def iden(message):
     bot.send_message(config.chatid, f'Твой ChatID - {message.chat.id}')
@@ -33,14 +71,28 @@ def la(message):
             bot.send_message(config.chatid, "Can't get CPU info")
         else:
             pass
-
+'''
 @bot.message_handler(content_types=["text"])
 def main(message):
    if message.chat.id == config.chatid:
       comand = message.text  #текст сообщения
       try: #если команда невыполняемая - check_output выдаст exception
-         bot.send_message(message.chat.id, check_output(comand, shell = True))
+         bot.send_message(message.chat.id, check_output(comand, shell=True))
       except:
-         bot.send_message(message.chat.id, "Invalid input") #если команда некорректна
+         bot.send_message(message.chat.id, "Команда введена некорректно") #если команда некорректна
+
+
+# Мониторинг оперативной памяти
+MEMORY_THRESHOLD = 80
+def check_memory_usage():
+    memory = psutil.virtual_memory()
+    memory_percent = memory.percent
+    if memory_percent >= MEMORY_THRESHOLD:
+        message = f'Оперативка слишком загружена бро!: {memory_percent}%'
+        bot.send_message(config.chatid, text=message)
+
+if __name__ == '__main__':
+    check_memory_usage()
+
 
 bot.polling()
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
+import datetime
 import os
 import psutil
+from subprocess import check_output
 
-print(psutil.getloadavg())
-print(psutil.disk_usage('/dev/nvme0n1p2'))
-print(psutil.version_info)
-print(psutil.virtual_memory())
+a = check_output(args ='cat /proc/cpuinfo', shell = True)
+print(a)
+
+b = psutil.getloadavg()
+b = list(b)
+print(b[0:3])
+
+c = psutil.disk_usage('/')
+print(c)
+c = list(psutil.disk_usage('/'))
+print(c)
+c = f'Диск занят на - {c[3]} %, доступно {int(c[2]/1024/1024/1024)} Гб'
+
+print(c)
+
+e = check_output(args='uptime -p', shell=True)
+print(e)
